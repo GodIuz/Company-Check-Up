1c28f2d2fadb3c761f29299469cf0edec6f1b966
NotYezz <notyestv@gmail.com>
2023-11-10 05:47:05 +0200

    123

--- app/src/main/java/com/droidgeniuslabs/companycheck_up/MainActivity.java
+++ app/src/main/java/com/droidgeniuslabs/companycheck_up/MainActivity.java
@@ -1,15 +1,7 @@
 package com.droidgeniuslabs.companycheck_up;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
